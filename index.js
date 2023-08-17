14e30c47ba891420f7fb2bbfa50a7d25373713f7
AkameBYTES <foreverdavi47@gmail.com>
2023-08-16 21:58:35 -0300

    Fazendo aula de variaveis

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+Execicío 1
+Eu acho que nesse código, o console copiará somente o "b", já que não deram consolo.log no "a"
+Exercicío 2
+Eu acho que no fim todos virarão 10 (lol)
+Exercicío 3
+Nesse código, ele exibirá quanta horas você trabalha e quanto você recebe por dia
+mas na minha opinião, pelo o que entendi esse código deveria ter explicado o quanto ele receber e trabalha para mim saber detalhadamente.
+Enfim, acho eu que esse código está certo.
+
+*/
+
+let nome 
+let idade 
+
+typeof nome
+typeof idade
+
+console.log(typeof nome, typeof idade)
+/* porque se alguém pode mudar o nome e idade, já que foi utilizado o código "let" e não "const".
+*/
+
+
+
+prompt ("Qual o seu nome")
+prompt ("Qual a sua idade?")
+
+
+
+
+
+
+
+
+
+
